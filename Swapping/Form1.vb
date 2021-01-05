22fad21dd485a34c2606f5e9d741f05cc81a6a1c
ayushegupta <ayushigupta1414@gmail.com>
2021-01-05 13:45:55 +0530

    1st commit

---
+++ Swapping/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+
+    Private Sub temp_Click(sender As Object, e As EventArgs) Handles temp_pic.Click
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        temp_pic.Image = PictureBox1.Image
+        PictureBox1.Image = PictureBox2.Image
+        PictureBox2.Image = PictureBox3.Image
+        PictureBox3.Image = temp_pic.Image
+    End Sub
+
+    Private Sub PictureBox3_Click(sender As Object, e As EventArgs) Handles PictureBox3.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
